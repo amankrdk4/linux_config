de8466b09778ee05152618a891ae4feafe1ee62d
sauron <sauron@onering>
2025-10-29 04:15:33 +0530

    lunarvim updated with tabstops

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -8,19 +8,39 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
-    -- F5: compile with input from inp.txt
+    -- F5: compile and run with input from inp.txt
     vim.api.nvim_buf_set_keymap(0, "n", "<F5>",
-      ":w <bar> !g++ -O2 % -o %:r && ./%:r < ./inp.txt<CR>",
+      ":w <bar> !g++ -std=c++17 -O2 -DLOCAL % -o %:r && ./%:r < ./inp.txt<CR>",
       { noremap = true, silent = true }
     )
-    -- F6: compile without input redirection
+
+    -- F6: compile and run without input redirection
     vim.api.nvim_buf_set_keymap(0, "n", "<F6>",
-      ":w <bar> !g++ -O2 % -o %:r && ./%:r<CR>",
+      ":w <bar> !g++ -std=c++17 -O2 -DLOCAL % -o %:r && ./%:r<CR>",
       { noremap = true, silent = true }
     )
-    -- F7: overwrite inp.txt with clipboard content
+
+    -- F7: overwrite inp.txt with clipboard content (cross-platform)
     vim.api.nvim_buf_set_keymap(0, "n", "<F7>",
-      ":let @\" = system('xclip -o -selection clipboard') | call writefile(split(@\", '\\n'), 'inp.txt')<CR>",
+      ":lua vim.fn.writefile(vim.fn.split(vim.fn.getreg('+'), '\\n'), 'inp.txt')<CR>",
+      { noremap = true, silent = true }
+    )
+
+    -- F8: run existing executable with inp.txt (no recompilation)
+    vim.api.nvim_buf_set_keymap(0, "n", "<F8>",
+      ":!./%:r < ./inp.txt<CR>",
+      { noremap = true, silent = true }
+    )
+
+    -- F9: run and save output to out.txt
+    vim.api.nvim_buf_set_keymap(0, "n", "<F9>",
+      ":!./%:r < ./inp.txt > out.txt && echo 'Output written to out.txt'<CR>",
+      { noremap = true, silent = true }
+    )
+
+    -- F10: clean executable
+    vim.api.nvim_buf_set_keymap(0, "n", "<F10>",
+      ":!rm -f %:r && echo 'Cleaned executable'<CR>",
       { noremap = true, silent = true }
     )
   end,
@@ -29,21 +49,34 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Disable all built-in LSPs
 lvim.lsp.installer.setup.automatic_installation = false
 lvim.lsp.automatic_servers_installation = false
--- colorscheme change 
+
+-- Colorscheme
 lvim.colorscheme = "koehler"
--- relative number 
+
+-- Relative line numbers
 vim.opt.relativenumber = true
 
--- Enable only simple cursorline
-vim.opt.cursorline = true       -- highlight current line
-vim.opt.cursorcolumn = false    -- disable current column highlight
-vim.opt.termguicolors = true    -- ensure colors work well
-
--- Underline the current line using the cursor highlight
+-- Enable simple cursorline (underline only)
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = false
+vim.opt.termguicolors = true
 vim.cmd [[
   highlight CursorLine guibg=NONE guifg=NONE gui=underline
 ]]
--- Disable autocompletion
+
+-- Disable autocompletion (manual mode only)
 lvim.builtin.cmp.active = false
--- jj remap
+
+-- Insert-mode escape with jj
 vim.keymap.set("i", "jj", "<Esc>")
+
+-- Indentation preferences for C++
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Disable automatic comment continuation
+vim.cmd [[autocmd FileType * setlocal formatoptions-=cro]]
+
+
